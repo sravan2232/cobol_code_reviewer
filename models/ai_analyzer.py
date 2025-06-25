1af81b6dc3910235c3a9a34ec9e4d5d99ebb239b
Sravan Palle <68579813+sravan2232@users.noreply.github.com>
2025-06-25 09:18:20 +0530

    Update ai_analyzer.py

--- models/ai_analyzer.py
+++ models/ai_analyzer.py
@@ -2,21 +2,7 @@ import requests
 import openai
 import streamlit as st
 openai.api_key = st.secrets["OPENAI_API_KEY"]
-#from Model.openai_ai import ask_openai as ask_ai
-#def ask_local_ai(prompt, model='mistral'):
-#    url = "http://localhost:11434/api/generate"
-#    payload = {
-#        "model": model,
-#        "prompt": prompt,
-#        "stream": False
-#    }
 
-#    try:
-#        response = requests.post(url, json=payload)
-#        return response.json()['response'].strip()
-#    except Exception as e:
-#        return f"❌ Error communicating with local AI: {e}"
-# No need to import from Model.openai_ai anymore
 
 def ask_ai(prompt, model="gpt-3.5-turbo"):
     response = openai.ChatCompletion.create(
